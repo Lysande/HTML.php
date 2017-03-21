279cfa554e89869ddd6e40312a27b600d2343ed9
Emil Johnsen <johnsen.emil@gmail.com>
2017-03-21 21:54:04 +0100

    Add all the HTML elements as non-empty elements

--- lib/HTMLElements.php
+++ lib/HTMLElements.php
@@ -1,13 +1,694 @@
 <?php
-
   trait HTMLElements
   {
-    /**
-     * HTML tag specifics.
-     */
+    public static function a($attributes = [], $children = [])
+    {
+      return self::tag("a", $attributes, $children);
+    }
+
+    public static function abbr($attributes = [], $children = [])
+    {
+      return self::tag("abbr", $attributes, $children);
+    }
+
+    public static function acronym($attributes = [], $children = [])
+    {
+      return self::tag("acronym", $attributes, $children);
+    }
+
+    public static function address($attributes = [], $children = [])
+    {
+      return self::tag("address", $attributes, $children);
+    }
+
+    public static function applet($attributes = [], $children = [])
+    {
+      return self::tag("applet", $attributes, $children);
+    }
+
+    public static function area($attributes = [], $children = [])
+    {
+      return self::tag("area", $attributes, $children);
+    }
+
+    public static function article($attributes = [], $children = [])
+    {
+      return self::tag("article", $attributes, $children);
+    }
+
+    public static function aside($attributes = [], $children = [])
+    {
+      return self::tag("aside", $attributes, $children);
+    }
+
+    public static function audio($attributes = [], $children = [])
+    {
+      return self::tag("audio", $attributes, $children);
+    }
+
+    public static function b($attributes = [], $children = [])
+    {
+      return self::tag("b", $attributes, $children);
+    }
+
+    public static function base($attributes = [], $children = [])
+    {
+      return self::tag("base", $attributes, $children);
+    }
+
+    public static function basefont($attributes = [], $children = [])
+    {
+      return self::tag("basefont", $attributes, $children);
+    }
+
+    public static function bdi($attributes = [], $children = [])
+    {
+      return self::tag("bdi", $attributes, $children);
+    }
+
+    public static function bdo($attributes = [], $children = [])
+    {
+      return self::tag("bdo", $attributes, $children);
+    }
+
+    public static function bgsound($attributes = [], $children = [])
+    {
+      return self::tag("bgsound", $attributes, $children);
+    }
+
+    public static function big($attributes = [], $children = [])
+    {
+      return self::tag("big", $attributes, $children);
+    }
+
+    public static function blink($attributes = [], $children = [])
+    {
+      return self::tag("blink", $attributes, $children);
+    }
+
+    public static function blockquote($attributes = [], $children = [])
+    {
+      return self::tag("blockquote", $attributes, $children);
+    }
+
+    public static function body($attributes = [], $children = [])
+    {
+      return self::tag("body", $attributes, $children);
+    }
+
+    public static function br($attributes = [], $children = [])
+    {
+      return self::tag("br", $attributes, $children);
+    }
+
+    public static function button($attributes = [], $children = [])
+    {
+      return self::tag("button", $attributes, $children);
+    }
+
+    public static function canvas($attributes = [], $children = [])
+    {
+      return self::tag("canvas", $attributes, $children);
+    }
+
+    public static function caption($attributes = [], $children = [])
+    {
+      return self::tag("caption", $attributes, $children);
+    }
+
+    public static function center($attributes = [], $children = [])
+    {
+      return self::tag("center", $attributes, $children);
+    }
+
+    public static function cite($attributes = [], $children = [])
+    {
+      return self::tag("cite", $attributes, $children);
+    }
+
+    public static function code($attributes = [], $children = [])
+    {
+      return self::tag("code", $attributes, $children);
+    }
+
+    public static function col($attributes = [], $children = [])
+    {
+      return self::tag("col", $attributes, $children);
+    }
+
+    public static function colgroup($attributes = [], $children = [])
+    {
+      return self::tag("colgroup", $attributes, $children);
+    }
+
+    public static function command($attributes = [], $children = [])
+    {
+      return self::tag("command", $attributes, $children);
+    }
+
+    public static function content($attributes = [], $children = [])
+    {
+      return self::tag("content", $attributes, $children);
+    }
+
+    public static function data($attributes = [], $children = [])
+    {
+      return self::tag("data", $attributes, $children);
+    }
+
+    public static function datalist($attributes = [], $children = [])
+    {
+      return self::tag("datalist", $attributes, $children);
+    }
+
+    public static function dd($attributes = [], $children = [])
+    {
+      return self::tag("dd", $attributes, $children);
+    }
+
+    public static function del($attributes = [], $children = [])
+    {
+      return self::tag("del", $attributes, $children);
+    }
+
+    public static function details($attributes = [], $children = [])
+    {
+      return self::tag("details", $attributes, $children);
+    }
+
+    public static function dfn($attributes = [], $children = [])
+    {
+      return self::tag("dfn", $attributes, $children);
+    }
+
+    public static function dialog($attributes = [], $children = [])
+    {
+      return self::tag("dialog", $attributes, $children);
+    }
+
+    public static function dir($attributes = [], $children = [])
+    {
+      return self::tag("dir", $attributes, $children);
+    }
 
     public static function div($attributes = [], $children = [])
     {
       return self::tag("div", $attributes, $children);
+    }
+
+    public static function dl($attributes = [], $children = [])
+    {
+      return self::tag("dl", $attributes, $children);
+    }
+
+    public static function dt($attributes = [], $children = [])
+    {
+      return self::tag("dt", $attributes, $children);
+    }
+
+    public static function element($attributes = [], $children = [])
+    {
+      return self::tag("element", $attributes, $children);
+    }
+
+    public static function em($attributes = [], $children = [])
+    {
+      return self::tag("em", $attributes, $children);
+    }
+
+    public static function embed($attributes = [], $children = [])
+    {
+      return self::tag("embed", $attributes, $children);
+    }
+
+    public static function fieldset($attributes = [], $children = [])
+    {
+      return self::tag("fieldset", $attributes, $children);
+    }
+
+    public static function figcaption($attributes = [], $children = [])
+    {
+      return self::tag("figcaption", $attributes, $children);
+    }
+
+    public static function figure($attributes = [], $children = [])
+    {
+      return self::tag("figure", $attributes, $children);
+    }
+
+    public static function font($attributes = [], $children = [])
+    {
+      return self::tag("font", $attributes, $children);
+    }
+
+    public static function footer($attributes = [], $children = [])
+    {
+      return self::tag("footer", $attributes, $children);
+    }
+
+    public static function form($attributes = [], $children = [])
+    {
+      return self::tag("form", $attributes, $children);
+    }
+
+    public static function frame($attributes = [], $children = [])
+    {
+      return self::tag("frame", $attributes, $children);
+    }
+
+    public static function frameset($attributes = [], $children = [])
+    {
+      return self::tag("frameset", $attributes, $children);
+    }
+
+    public static function h1–h6($attributes = [], $children = [])
+    {
+      return self::tag("h1–h6", $attributes, $children);
+    }
+
+    public static function head($attributes = [], $children = [])
+    {
+      return self::tag("head", $attributes, $children);
+    }
+
+    public static function header($attributes = [], $children = [])
+    {
+      return self::tag("header", $attributes, $children);
+    }
+
+    public static function hgroup($attributes = [], $children = [])
+    {
+      return self::tag("hgroup", $attributes, $children);
+    }
+
+    public static function hr($attributes = [], $children = [])
+    {
+      return self::tag("hr", $attributes, $children);
+    }
+
+    public static function html($attributes = [], $children = [])
+    {
+      return self::tag("html", $attributes, $children);
+    }
+
+    public static function i($attributes = [], $children = [])
+    {
+      return self::tag("i", $attributes, $children);
+    }
+
+    public static function iframe($attributes = [], $children = [])
+    {
+      return self::tag("iframe", $attributes, $children);
+    }
+
+    public static function image($attributes = [], $children = [])
+    {
+      return self::tag("image", $attributes, $children);
+    }
+
+    public static function img($attributes = [], $children = [])
+    {
+      return self::tag("img", $attributes, $children);
+    }
+
+    public static function input($attributes = [], $children = [])
+    {
+      return self::tag("input", $attributes, $children);
+    }
+
+    public static function ins($attributes = [], $children = [])
+    {
+      return self::tag("ins", $attributes, $children);
+    }
+
+    public static function isindex($attributes = [], $children = [])
+    {
+      return self::tag("isindex", $attributes, $children);
+    }
+
+    public static function kbd($attributes = [], $children = [])
+    {
+      return self::tag("kbd", $attributes, $children);
+    }
+
+    public static function keygen($attributes = [], $children = [])
+    {
+      return self::tag("keygen", $attributes, $children);
+    }
+
+    public static function label($attributes = [], $children = [])
+    {
+      return self::tag("label", $attributes, $children);
+    }
+
+    public static function legend($attributes = [], $children = [])
+    {
+      return self::tag("legend", $attributes, $children);
+    }
+
+    public static function li($attributes = [], $children = [])
+    {
+      return self::tag("li", $attributes, $children);
+    }
+
+    public static function link($attributes = [], $children = [])
+    {
+      return self::tag("link", $attributes, $children);
+    }
+
+    public static function listing($attributes = [], $children = [])
+    {
+      return self::tag("listing", $attributes, $children);
+    }
+
+    public static function main($attributes = [], $children = [])
+    {
+      return self::tag("main", $attributes, $children);
+    }
+
+    public static function map($attributes = [], $children = [])
+    {
+      return self::tag("map", $attributes, $children);
+    }
+
+    public static function mark($attributes = [], $children = [])
+    {
+      return self::tag("mark", $attributes, $children);
+    }
+
+    public static function marquee($attributes = [], $children = [])
+    {
+      return self::tag("marquee", $attributes, $children);
+    }
+
+    public static function menu($attributes = [], $children = [])
+    {
+      return self::tag("menu", $attributes, $children);
+    }
+
+    public static function menuitem($attributes = [], $children = [])
+    {
+      return self::tag("menuitem", $attributes, $children);
+    }
+
+    public static function meta($attributes = [], $children = [])
+    {
+      return self::tag("meta", $attributes, $children);
+    }
+
+    public static function meter($attributes = [], $children = [])
+    {
+      return self::tag("meter", $attributes, $children);
+    }
+
+    public static function multicol($attributes = [], $children = [])
+    {
+      return self::tag("multicol", $attributes, $children);
+    }
+
+    public static function nav($attributes = [], $children = [])
+    {
+      return self::tag("nav", $attributes, $children);
+    }
+
+    public static function nobr($attributes = [], $children = [])
+    {
+      return self::tag("nobr", $attributes, $children);
+    }
+
+    public static function noembed($attributes = [], $children = [])
+    {
+      return self::tag("noembed", $attributes, $children);
+    }
+
+    public static function noframes($attributes = [], $children = [])
+    {
+      return self::tag("noframes", $attributes, $children);
+    }
+
+    public static function noscript($attributes = [], $children = [])
+    {
+      return self::tag("noscript", $attributes, $children);
+    }
+
+    public static function object($attributes = [], $children = [])
+    {
+      return self::tag("object", $attributes, $children);
+    }
+
+    public static function ol($attributes = [], $children = [])
+    {
+      return self::tag("ol", $attributes, $children);
+    }
+
+    public static function optgroup($attributes = [], $children = [])
+    {
+      return self::tag("optgroup", $attributes, $children);
+    }
+
+    public static function option($attributes = [], $children = [])
+    {
+      return self::tag("option", $attributes, $children);
+    }
+
+    public static function output($attributes = [], $children = [])
+    {
+      return self::tag("output", $attributes, $children);
+    }
+
+    public static function p($attributes = [], $children = [])
+    {
+      return self::tag("p", $attributes, $children);
+    }
+
+    public static function param($attributes = [], $children = [])
+    {
+      return self::tag("param", $attributes, $children);
+    }
+
+    public static function picture($attributes = [], $children = [])
+    {
+      return self::tag("picture", $attributes, $children);
+    }
+
+    public static function plaintext($attributes = [], $children = [])
+    {
+      return self::tag("plaintext", $attributes, $children);
+    }
+
+    public static function pre($attributes = [], $children = [])
+    {
+      return self::tag("pre", $attributes, $children);
+    }
+
+    public static function progress($attributes = [], $children = [])
+    {
+      return self::tag("progress", $attributes, $children);
+    }
+
+    public static function q($attributes = [], $children = [])
+    {
+      return self::tag("q", $attributes, $children);
+    }
+
+    public static function rp($attributes = [], $children = [])
+    {
+      return self::tag("rp", $attributes, $children);
+    }
+
+    public static function rt($attributes = [], $children = [])
+    {
+      return self::tag("rt", $attributes, $children);
+    }
+
+    public static function rtc($attributes = [], $children = [])
+    {
+      return self::tag("rtc", $attributes, $children);
+    }
+
+    public static function ruby($attributes = [], $children = [])
+    {
+      return self::tag("ruby", $attributes, $children);
+    }
+
+    public static function s($attributes = [], $children = [])
+    {
+      return self::tag("s", $attributes, $children);
+    }
+
+    public static function samp($attributes = [], $children = [])
+    {
+      return self::tag("samp", $attributes, $children);
+    }
+
+    public static function script($attributes = [], $children = [])
+    {
+      return self::tag("script", $attributes, $children);
+    }
+
+    public static function section($attributes = [], $children = [])
+    {
+      return self::tag("section", $attributes, $children);
+    }
+
+    public static function select($attributes = [], $children = [])
+    {
+      return self::tag("select", $attributes, $children);
+    }
+
+    public static function shadow($attributes = [], $children = [])
+    {
+      return self::tag("shadow", $attributes, $children);
+    }
+
+    public static function slot($attributes = [], $children = [])
+    {
+      return self::tag("slot", $attributes, $children);
+    }
+
+    public static function small($attributes = [], $children = [])
+    {
+      return self::tag("small", $attributes, $children);
+    }
+
+    public static function source($attributes = [], $children = [])
+    {
+      return self::tag("source", $attributes, $children);
+    }
+
+    public static function spacer($attributes = [], $children = [])
+    {
+      return self::tag("spacer", $attributes, $children);
+    }
+
+    public static function span($attributes = [], $children = [])
+    {
+      return self::tag("span", $attributes, $children);
+    }
+
+    public static function strike($attributes = [], $children = [])
+    {
+      return self::tag("strike", $attributes, $children);
+    }
+
+    public static function strong($attributes = [], $children = [])
+    {
+      return self::tag("strong", $attributes, $children);
+    }
+
+    public static function style($attributes = [], $children = [])
+    {
+      return self::tag("style", $attributes, $children);
+    }
+
+    public static function sub($attributes = [], $children = [])
+    {
+      return self::tag("sub", $attributes, $children);
+    }
+
+    public static function summary($attributes = [], $children = [])
+    {
+      return self::tag("summary", $attributes, $children);
+    }
+
+    public static function sup($attributes = [], $children = [])
+    {
+      return self::tag("sup", $attributes, $children);
+    }
+
+    public static function table($attributes = [], $children = [])
+    {
+      return self::tag("table", $attributes, $children);
+    }
+
+    public static function tbody($attributes = [], $children = [])
+    {
+      return self::tag("tbody", $attributes, $children);
+    }
+
+    public static function td($attributes = [], $children = [])
+    {
+      return self::tag("td", $attributes, $children);
+    }
+
+    public static function template($attributes = [], $children = [])
+    {
+      return self::tag("template", $attributes, $children);
+    }
+
+    public static function textarea($attributes = [], $children = [])
+    {
+      return self::tag("textarea", $attributes, $children);
+    }
+
+    public static function tfoot($attributes = [], $children = [])
+    {
+      return self::tag("tfoot", $attributes, $children);
+    }
+
+    public static function th($attributes = [], $children = [])
+    {
+      return self::tag("th", $attributes, $children);
+    }
+
+    public static function thead($attributes = [], $children = [])
+    {
+      return self::tag("thead", $attributes, $children);
+    }
+
+    public static function time($attributes = [], $children = [])
+    {
+      return self::tag("time", $attributes, $children);
+    }
+
+    public static function title($attributes = [], $children = [])
+    {
+      return self::tag("title", $attributes, $children);
+    }
+
+    public static function tr($attributes = [], $children = [])
+    {
+      return self::tag("tr", $attributes, $children);
+    }
+
+    public static function track($attributes = [], $children = [])
+    {
+      return self::tag("track", $attributes, $children);
+    }
+
+    public static function tt($attributes = [], $children = [])
+    {
+      return self::tag("tt", $attributes, $children);
+    }
+
+    public static function u($attributes = [], $children = [])
+    {
+      return self::tag("u", $attributes, $children);
+    }
+
+    public static function ul($attributes = [], $children = [])
+    {
+      return self::tag("ul", $attributes, $children);
+    }
+    /**
+     * `var` is a reserved word in PHP. 
+     */
+    public static function _var($attributes = [], $children = [])
+    {
+      return self::tag("var", $attributes, $children);
+    }
+    public static function video($attributes = [], $children = [])
+    {
+      return self::tag("video", $attributes, $children);
+    }
+
+    public static function wbr($attributes = [], $children = [])
+    {
+      return self::tag("wbr", $attributes, $children);
+    }
+
+    public static function xmp($attributes = [], $children = [])
+    {
+      return self::tag("xmp", $attributes, $children);
     }
   }
